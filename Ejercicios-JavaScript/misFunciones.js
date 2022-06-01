a74f25814513404453f36c3b31563e9c6c4c60c9
MaximoChattas <maxichattas@gmail.com>
2022-06-01 09:46:46 -0300

    Agrego ejercicio dibujar auto

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,57 +5,50 @@
  * @param {Number} valor - Valor de los inputs del formulario
  */
 
-function conversionUnidades(id , valor)
-{
+function conversionUnidades(id, valor) {
     let metro, pie, pulgada, yarda;
 
-    if(valor.includes(','))
-    {
-        valor = valor.replace(',' , '.');
+    if (valor.includes(',')) {
+        valor = valor.replace(',', '.');
     }
 
-    if(isNaN(valor))
-    {
+    if (isNaN(valor)) {
         alert("Ingrese un numero");
         metro = "";
         pie = "";
         pulgada = "";
         yarda = "";
 
-    } else if (id === "metro")
-    {
+    } else if (id === "metro") {
         metro = valor;
-        pulgada = valor*39.3701;
-        yarda = valor*1.09361;
-        pie = valor*3.28084;
+        pulgada = valor * 39.3701;
+        yarda = valor * 1.09361;
+        pie = valor * 3.28084;
 
-    } else if (id === "pulgada")
-    {
+    } else if (id === "pulgada") {
         pulgada = valor;
-        metro = valor*0.0254;
-        yarda = valor*0.0277778;
-        pie = valor*0.0833333;
+        metro = valor * 0.0254;
+        yarda = valor * 0.0277778;
+        pie = valor * 0.0833333;
 
-    } else if (id === "yarda")
-    {
+    } else if (id === "yarda") {
         yarda = valor;
-        metro = valor*0.9144;
-        pulgada = valor*36;
-        pie = valor*3;
+        metro = valor * 0.9144;
+        pulgada = valor * 36;
+        pie = valor * 3;
 
-    } else if (id === "pie")
-    {
+    } else if (id === "pie") {
         pie = valor;
-        yarda = valor*0.333333;
-        metro = valor*0.3048;
-        pulgada = valor*12;
+        yarda = valor * 0.333333;
+        metro = valor * 0.3048;
+        pulgada = valor * 12;
 
     }
 
-    document.conversorUnidades.unid_metro.value = Math.round(metro*100)/100;
-    document.conversorUnidades.unid_pulgada.value = Math.round(pulgada*100)/100;
-    document.conversorUnidades.unid_yarda.value = Math.round(yarda*100)/100;
-    document.conversorUnidades.unid_pie.value = Math.round(pie*100)/100;
+    document.conversorUnidades.unid_metro.value = Math.round(metro * 100) / 100;
+    document.conversorUnidades.unid_pulgada.value = Math.round(pulgada * 100) / 100;
+    document.conversorUnidades.unid_yarda.value = Math.round(yarda * 100) / 100;
+    document.conversorUnidades.unid_pie.value = Math.round(pie * 100) / 100;
 }
 
 /**
@@ -64,19 +57,16 @@ function conversionUnidades(id , valor)
  * @param {String} id - ID de los inputs del formulario
  */
 
-function conversionAngulos(id) /*Verificar Funcionalidad*/
-{
-    let grados , rad;
+function conversionAngulos(id) /*Verificar Funcionalidad*/ {
+    let grados, rad;
 
-    if (id == "grados")
-    {
-       grados = document.getElementById("grados").value;
-        rad = (grados*Math.PI/180);
+    if (id == "grados") {
+        grados = document.getElementById("grados").value;
+        rad = (grados * Math.PI / 180);
 
-    }else if (id == "radianes")
-    {
+    } else if (id == "radianes") {
         rad = document.getElementById("radianes").value;
-        grados = (rad*180/Math.PI);
+        grados = (rad * 180 / Math.PI);
     }
 
     document.getElementById("grados").value = grados;
@@ -90,13 +80,10 @@ function conversionAngulos(id) /*Verificar Funcionalidad*/
  * @param {string} valor - Valor de los botones del formulario
  */
 
-function mostrarOcultar (valor)
-{
-    if (valor === "val_mostrar")
-    {
+function mostrarOcultar(valor) {
+    if (valor === "val_mostrar") {
         document.getElementById("div1").style.display = "block";
-    } else if (valor === "val_ocultar")
-    {
+    } else if (valor === "val_ocultar") {
         document.getElementById("div1").style.display = "none";
     }
 }
@@ -106,9 +93,8 @@ function mostrarOcultar (valor)
  * @method fnSuma
  */
 
-function fnSuma()
-{
-    let num1 , num2;
+function fnSuma() {
+    let num1, num2;
 
     num1 = document.getElementById("nums1").value;
     num2 = document.getElementById("nums2").value;
@@ -121,9 +107,8 @@ function fnSuma()
  * @method fnResta
  */
 
-function fnResta()
-{
-    let num1 , num2;
+function fnResta() {
+    let num1, num2;
 
     num1 = document.getElementById("numr1").value;
     num2 = document.getElementById("numr2").value;
@@ -136,9 +121,8 @@ function fnResta()
  * @method fnProducto
  */
 
-function fnProducto()
-{
-    let num1 , num2;
+function fnProducto() {
+    let num1, num2;
 
     num1 = document.getElementById("numm1").value;
     num2 = document.getElementById("numm2").value;
@@ -151,9 +135,8 @@ function fnProducto()
  * @method fnDivision
  */
 
-function fnDivision()
-{
-    let num1 , num2;
+function fnDivision() {
+    let num1, num2;
 
     num1 = document.getElementById("numd1").value;
     num2 = document.getElementById("numd2").value;
@@ -166,9 +149,8 @@ function fnDivision()
  * @method cargarWeb
  */
 
-function cargarWeb()
-{
-    let cantidad , unidad , URL;
+function cargarWeb() {
+    let cantidad, unidad, URL;
 
     cantidad = document.getElementById("distancia").value;
     unidad = document.getElementsByName("unidades")[0].value;
@@ -182,9 +164,8 @@ function cargarWeb()
  * @method cargarResultado
  */
 
-function cargarResultado()
-{
-    let cantidad , unidad , URL;
+function cargarResultado() {
+    let cantidad, unidad, URL;
 
     URL = window.location.href;
     URL = URL.split("#");
@@ -201,8 +182,7 @@ function cargarResultado()
  * @method dibujarCirculoCuadrado
  */
 
-function dibujarCirculoCuadrado()
-{
+function dibujarCirculoCuadrado() {
     let canvas = document.getElementById("myCanvas");
     let context = canvas.getContext("2d");
 
@@ -211,9 +191,9 @@ function dibujarCirculoCuadrado()
     let squareSize = 50;
     let margin = 15;
 
-    context.fillRect(margin ,canvasHeight-margin-squareSize , squareSize , squareSize);
+    context.fillRect(margin, canvasHeight - margin - squareSize, squareSize, squareSize);
 
-    context.arc(canvasWidth/2 , canvasHeight/2 , 100 , 0 , 2*Math.PI);
+    context.arc(canvasWidth / 2, canvasHeight / 2, 100, 0, 2 * Math.PI);
     context.stroke();
     context.fillStyle = "#00FF00";
     context.fill();
@@ -226,21 +206,26 @@ function dibujarCirculoCuadrado()
 
 function cargarListener() {
 
-    document.getElementById("canvasPaint").addEventListener("mousemove" , function (event){let canvas = document.getElementById("canvasPaint");
+    document.getElementById("canvasPaint").addEventListener("mousemove", function (event) {
+        let canvas = document.getElementById("canvasPaint");
         let context = canvas.getContext("2d");
 
         let posX = event.clientX;
         let posY = event.clientY;
 
-        canvas.onmousedown = function (){bandera = true};
-        canvas.onmouseup = function (){bandera = false};
+        canvas.onmousedown = function () {
+            bandera = true
+        };
+        canvas.onmouseup = function () {
+            bandera = false
+        };
 
-        console.log(posX , posY);
+        console.log(posX, posY);
 
-        if (bandera)
-        {
-            context.fillRect(posX-10 , posY-10 , 5 , 5);
-        }})
+        if (bandera) {
+            context.fillRect(posX - 10, posY - 10, 5, 5);
+        }
+    })
 }
 
 /**
@@ -248,8 +233,7 @@ function cargarListener() {
  * @method dibujarCirculoCuadrado
  */
 
-function limpiarCanvas()
-{
+function limpiarCanvas() {
     let canvas = document.getElementById("canvasPaint");
     let context = canvas.getContext("2d");
 
@@ -257,25 +241,28 @@ function limpiarCanvas()
 
 }
 
+/**
+ * Al hacer click sobre el boton, dibuja un cuadriculado sobre el canvas
+ * @method dibujarCuadriculado
+ */
+
 function dibujarCuadriculado() {
     let canvas = document.getElementById("myCanvas");
     let context = canvas.getContext("2d");
 
-    for (let i = 20 ; i < canvas.height ; i+=20)
-    {
+    for (let i = 20; i < canvas.height; i += 20) {
         context.beginPath();
-        context.moveTo(0 , i);
-        context.lineTo (canvas.width , i);
+        context.moveTo(0, i);
+        context.lineTo(canvas.width, i);
         context.lineWidth = 0.5;
         context.stroke();
         context.closePath();
     }
 
-    for (let i = 20 ; i < canvas.width ; i+=20)
-    {
+    for (let i = 20; i < canvas.width; i += 20) {
         context.beginPath();
-        context.moveTo(i , 0);
-        context.lineTo (i , canvas.height);
+        context.moveTo(i, 0);
+        context.lineTo(i, canvas.height);
         context.lineWidth = 0.5;
         context.strokeStyle = "#ff0000";
         context.stroke();
@@ -284,8 +271,8 @@ function dibujarCuadriculado() {
 
     //Eje X
     context.beginPath();
-    context.moveTo(0 , canvas.height/2);
-    context.lineTo(canvas.width , canvas.height/2);
+    context.moveTo(0, canvas.height / 2);
+    context.lineTo(canvas.width, canvas.height / 2);
     context.lineWidth = 1;
     context.strokeStyle = "#000000";
     context.stroke();
@@ -293,10 +280,33 @@ function dibujarCuadriculado() {
 
     //Eje Y
     context.beginPath();
-    context.moveTo(canvas.width/2 , 0);
-    context.lineTo(canvas.width/2 , canvas.height);
+    context.moveTo(canvas.width / 2, 0);
+    context.lineTo(canvas.width / 2, canvas.height);
     context.lineWidth = 1;
     context.strokeStyle = "#000000";
     context.stroke();
     context.closePath();
+}
+
+/**
+ * Al hacer click sobre el boton, dibuja un auto en las coordenadas ingresadas
+ * @method dibujarAuto
+ * @param {number} posX - Coordenada X
+ * @param {number} posY - Coordenada Y
+ */
+
+function dibujarAuto(posX , posY) {
+    let canvas = document.getElementById("myCanvas");
+    let context = canvas.getContext("2d");
+
+    let img;
+    img = new Image();
+    img.src = "images/auto.png";
+
+    canvas.width = canvas.width;
+
+    img.onload = function () {
+        context.drawImage(img , posX , posY);
+    }
+
 }
